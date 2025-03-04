0e1c76c29b1324358d7c08a4ea0f65d22f62dacb
Oluwaleye Victor <oluwasquarelsv@gmail.com>
2025-03-04 22:41:19 +0100

    feat: setup borrow book event consumer

--- admin_service/app/events.py
+++ admin_service/app/events.py
@@ -1,9 +1,11 @@
 import redis
 import json
 from sqlmodel import Session, select
-from app.api.deps import get_db
+from app.core.db import engine
 from app.core.config import settings
 from app.models import User
+from typing import List
+from app.models import Book, BorrowedBook, BorrowedBookCreate
 
 redis_client = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=0)
 
@@ -25,27 +27,55 @@ def listen_for_events():
             if event_data["event"] == "user_enrolled":
                 print(f"User with email {event_data['user']['email']} enrolled.")
                 enroll_user(event_data["user"])
+            if event_data["event"] == "new_borrowed_book":
+
+                borrow_book(event_data["data"])
 
 
 def enroll_user(payload: dict):
-    session = next(get_db())
-    try:
+
+    with Session(engine) as session:
         statement = select(User).where(User.email == payload.get("email"))
         user_exist = session.exec(statement).first()
         if user_exist:
             print(f"User with email {payload.get('email')} already enrolled.")
             return None
 
-        print(f"payload=>{payload}")
         user_create = User.model_validate(payload)
-        print(f"create user=>{user_create}")
+
         session.add(user_create)
         session.commit()
         session.refresh(user_create)
         print(f"User with email {payload.get('email')} enrolled.")
         return user_create
-    except Exception as e:
-        print(f"Error: {e}")
 
-    finally:
-        session.close()
+
+def borrow_book(payload: List[dict]):
+    print(f"New borrowed book: {payload}")
+    with Session(engine) as session:
+        create_data = []
+        for item in payload:
+            statement = select(Book).where(Book.id == item["book_id"])
+            book = session.exec(statement).first()
+            if not book:
+                print(f"Book with id {item['book_id']} not found.")
+                continue
+            borrow_book_data = BorrowedBookCreate(**item)
+            db_borrowed_book = BorrowedBook.model_validate(
+                borrow_book_data,
+                update={
+                    "user_id": item["user_id"],
+                    "return_date": item["return_date"],
+                    "book_id": book.id,
+                    "period_in_days": item["period_in_days"],
+                },
+            )
+
+            book.sqlmodel_update(
+                {"is_available": False, "available_date": item["return_date"]}
+            )
+            create_data.append(db_borrowed_book)
+
+        session.add_all(create_data)
+        session.commit()
+        print(f"NEW books({len(create_data)}) uploaded")
