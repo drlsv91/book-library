75e8b51296a91f2c4ace5a200e94fb691be21833
Oluwaleye Victor <oluwasquarelsv@gmail.com>
2025-03-04 22:44:15 +0100

    feat: publish delete book event

--- admin_service/app/api/routes/book.py
+++ admin_service/app/api/routes/book.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from app.models import BookPublic, BookCreate, Book, BooksPublic
-from typing import Annotated
-from app.api.deps import SessionDep, CurrentUser
+
+from app.api.deps import SessionDep, CurrentUser, get_current_user
 from typing import Any
 import uuid
-from sqlmodel import select, func
+from sqlmodel import select, func, desc
 from app.events import publish_event
 
 router = APIRouter(prefix="/books", tags=["Book"])
@@ -15,7 +15,7 @@ def add_book(
     *,
     session: SessionDep,
     current_user: CurrentUser,
-    book_data: Annotated[BookCreate, Depends()],
+    book_data: BookCreate,
 ) -> Any:
     """
     Add new Book.
@@ -30,11 +30,10 @@ def add_book(
     return db_book
 
 
-@router.get("/", response_model=BooksPublic)
+@router.get("/", dependencies=[Depends(get_current_user)], response_model=BooksPublic)
 def read_books(
     *,
     session: SessionDep,
-    current_user: CurrentUser,
     is_available: bool | None = None,
     skip: int = 0,
     limit: int = 100,
@@ -49,16 +48,16 @@ def read_books(
     if is_available is not None:
         query = query.where(Book.is_available == is_available)
 
-    statement = query.offset(skip).limit(limit)
+    statement = query.order_by(desc(Book.created_at)).offset(skip).limit(limit)
     books = session.exec(statement).all()
 
     return BooksPublic(data=books, count=count)
 
 
-@router.delete("/{book_id}", response_model=BookPublic)
-def delete_book(
-    *, session: SessionDep, current_user: CurrentUser, book_id: uuid.UUID
-) -> Any:
+@router.delete(
+    "/{book_id}", dependencies=[Depends(get_current_user)], response_model=BookPublic
+)
+def delete_book(*, session: SessionDep, book_id: uuid.UUID) -> Any:
     """
     Delete a Book by ID
     """
@@ -71,4 +70,8 @@ def delete_book(
 
     session.delete(db_book)
     session.commit()
+    publish_event(
+        "admin_events",
+        {"event": "delete_book", "data": db_book.model_dump(mode="json")},
+    )
     return db_book
