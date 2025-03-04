2464afb567c8ce10c3219577c1b511b4ee635e09
Oluwaleye Victor <oluwasquarelsv@gmail.com>
2025-03-04 22:45:25 +0100

    chore: update prestart migration script

--- admin_service/scripts/prestart.sh
+++ admin_service/scripts/prestart.sh
@@ -3,14 +3,10 @@
 set -e
 set -x
 
-# Sync dependencies
-echo "Running uv sync..."
-uv sync
+# Let the DB start
+echo "Running test sync..."
+python app/prestart.py
 
-# Run Alembic migrations (if alembic directory exists)
-if [ -d "/app/alembic" ]; then
-    echo "Running Alembic migrations..."
-    alembic upgrade head
-else
-    echo "Alembic directory not found. Skipping migrations."
-fi
+# Run migrations
+echo "Running migration"
+alembic upgrade head
