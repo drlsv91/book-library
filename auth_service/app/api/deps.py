c08f5b41b8e682b63997e7bc2dd1378d38d678c4
Oluwaleye Victor <oluwasquarelsv@gmail.com>
2025-03-05 12:47:15 +0100

    fix: cast token.sub to uuid query comparism

--- auth_service/app/api/deps.py
+++ auth_service/app/api/deps.py
@@ -10,6 +10,7 @@ from app.core import security
 from app.core.config import settings
 from app.core.db import engine
 from app.models import TokenPayload, User
+import uuid
 
 reusable_oauth2 = OAuth2PasswordBearer(
     tokenUrl=f"{settings.API_V1_STR}/login/access-token"
@@ -36,7 +37,7 @@ def get_current_user(session: SessionDep, token: TokenDep) -> User:
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Could not validate credentials",
         )
-    user = session.get(User, token_data.sub)
+    user = session.get(User, uuid.UUID(token_data.sub))
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     if not user.is_active:
